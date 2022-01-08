e684eae7bbe4d0e93d061ab49d59f68ec74b46ca
David Fokkema <davidfokkema@icloud.com>
2022-01-08 21:50:37 +0100

    Firmware: use BSEC lib with BME680
    Copied from example code

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,12 +1,22 @@
 #include <ArduinoBLE.h>
 #include "Adafruit_MCP9808.h"
+#include "bsec.h"
 
 
 BLEService envService("181A");
 BLEFloatCharacteristic temperatureCharacteristic("2A6E", BLERead | BLENotify);
 BLEDescriptor temperatureDescriptor("2901", "Ambient temperature (ºC)");
 
+BLEFloatCharacteristic pressureCharacteristic("2A6D", BLERead | BLENotify);
+BLEDescriptor pressureDescriptor("2901", "Atmospheric pressure (hPa)");
+BLEFloatCharacteristic humidityCharacteristic("2A6F", BLERead | BLENotify);
+BLEDescriptor humidityDescriptor("2901", "Relative humidity (%)");
+BLEFloatCharacteristic eCO2Characteristic("2BD3", BLERead | BLENotify);
+BLEDescriptor eCO2Descriptor("2901", "Equivalent CO2 (ppm)");
+
+
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+Bsec iaqSensor;
 
 unsigned long lastMeasurementTime = 0;
 
@@ -18,7 +28,13 @@ void setup() {
   BLE.setDeviceName("Arduino EnvSense");
 
   envService.addCharacteristic(temperatureCharacteristic);
+  envService.addCharacteristic(pressureCharacteristic);
+  envService.addCharacteristic(humidityCharacteristic);
+  envService.addCharacteristic(eCO2Characteristic);
   temperatureCharacteristic.addDescriptor(temperatureDescriptor);
+  pressureCharacteristic.addDescriptor(pressureDescriptor);
+  humidityCharacteristic.addDescriptor(humidityDescriptor);
+  eCO2Characteristic.addDescriptor(eCO2Descriptor);
   BLE.addService(envService);
 
   BLE.setAdvertisedService(envService);
@@ -31,18 +47,33 @@ void setup() {
   //  1    0.25°C      65 ms
   //  2    0.125°C     130 ms
   //  3    0.0625°C    250 ms
+
+  Wire.begin();
+  iaqSensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
+  bsec_virtual_sensor_t sensorList[3] = {
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_CO2_EQUIVALENT,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+  };
+  iaqSensor.updateSubscription(sensorList, 3, BSEC_SAMPLE_RATE_LP);
 }
 
 void loop() {
   unsigned long now;
   
-  // put your main code here, to run repeatedly:
   BLE.poll();
+  
   now = millis();
   if ((now - lastMeasurementTime) > 1000) {
     lastMeasurementTime = now;
     tempsensor.wake();
     temperatureCharacteristic.writeValue(tempsensor.readTempC());
     tempsensor.shutdown_wake(1);
+  }
+
+  if (iaqSensor.run()) {
+    pressureCharacteristic.writeValue(iaqSensor.pressure / 100);
+    humidityCharacteristic.writeValue(iaqSensor.humidity);
+    eCO2Characteristic.writeValue(iaqSensor.co2Equivalent);
   }
 }
