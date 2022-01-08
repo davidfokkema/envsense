8e5b7507d9e3794bdf90b043929ea7d051b0ce06
David Fokkema <davidfokkema@icloud.com>
2022-01-08 13:55:54 +0100

    Scanning runs in new thread
    No more blocking!

--- src/envsense/app.py
+++ src/envsense/app.py
@@ -1,6 +1,6 @@
 import asyncio
 import struct
-import time
+import threading
 import uuid
 
 import bleak
@@ -13,21 +13,26 @@ TEMP_UUID = uuid.UUID(int=BASE_UUID + (0x2A6E << 96))
 
 def sync(func):
     def f(*args, **kwargs):
-        loop = asyncio.get_event_loop()
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
         loop.run_until_complete(func(*args, **kwargs))
 
     return f
 
 
 class AwesomeStatusBarApp(rumps.App):
+
+    device = None
+    scan_thread = None
+
     def __init__(self):
         super(AwesomeStatusBarApp, self).__init__("EnvSense")
 
         self.menu_device = rumps.MenuItem("Searching for device...")
         self.menu_temp = rumps.MenuItem("Waiting for data...")
         self.menu = [self.menu_device, self.menu_temp]
-
-        self.device = None
 
     @rumps.timer(2)
     @sync
@@ -41,13 +46,23 @@ class AwesomeStatusBarApp(rumps.App):
                 self.title = f"{temperature:.1f} ºC"
 
     @rumps.timer(5)
+    def start_scan_if_needed(self, sender):
+        print("Start scan")
+        if self.device is None and (
+            self.scan_thread is None or not self.scan_thread.is_alive()
+        ):
+            self.scan_thread = threading.Thread(target=self.scan_devices)
+            self.scan_thread.start()
+        print("Done")
+
     @sync
-    async def find_device(self, sender):
-        if self.device is None:
-            devices = await bleak.BleakScanner.discover()
-            for dev in devices:
-                if DEVNAME in dev.name:
-                    self.device = dev
+    async def scan_devices(self):
+        print("Scanning...")
+        devices = await bleak.BleakScanner.discover()
+        for dev in devices:
+            if DEVNAME in dev.name:
+                self.device = dev
+        print("Scanning done.")
 
     # @rumps.clicked("Preferences")
     # def prefs(self, _):
